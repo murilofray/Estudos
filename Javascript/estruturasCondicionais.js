b1ecdeada7cba4bac2342a93369a6b434b8d89da
Murilo <murilofray>
2023-12-20 10:00:35 -0300

    Estrutura Condicional IF ELSE IF ELSE

--- Javascript/estruturasCondicionais.js
+++ Javascript/estruturasCondicionais.js
@@ -1,7 +1,13 @@
 var numero = 7;
-//Divido o numero por 2 pego o resto da divisão
-//Se for 0 o numero é par se não impar
-//Se for par retorna true se não false
-const numeroEhPar = numero%2 == 0;
 
-console.log(numeroEhPar);
+const numeroEhPositivo = numero > 0;
+
+//IF ELSE IF ELSE
+if(numero == 0) {
+   console.log("Neutro");
+}
+else if(numeroEhPositivo){
+  console.log("Positivo")
+}else{
+  console.log("Negativo")
+}
