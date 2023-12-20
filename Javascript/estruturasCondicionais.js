8738e7f5003a60477e0dc1e5a08d92827b1f9c42
Murilo <murilofray>
2023-12-20 08:10:35 -0300

    Operadores em javascript

---
+++ Javascript/estruturasCondicionais.js
@@ -0,0 +1,7 @@
+var numero = 7;
+//Divido o numero por 2 pego o resto da divisão
+//Se for 0 o numero é par se não impar
+//Se for par retorna true se não false
+const numeroEhPar = numero%2 == 0;
+
+console.log(numeroEhPar);
